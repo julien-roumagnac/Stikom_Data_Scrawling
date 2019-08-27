a0593437548403db99ecad3f752aa13cd23964e2
julien-roumagnac <julienroumagnac1@gmail.com>
2019-08-27 11:26:29 +0200

    No newsletter errror fixed

--- TRIGGERING_NEWSLETTER.py
+++ TRIGGERING_NEWSLETTER.py
@@ -13,87 +13,90 @@ def trigger_and_mail():
     # retrieval of publication data from the DB
 
     publicationsDf = pandas.read_sql(("SELECT * FROM publication order by creation_date "),newsletter_db)
-    print('ALL PUBLICATIONS ALLREADY SCRAWLED ORDER BY DATE')
-    print()
-    for publi in publicationsDf.values :
-        print(publi[2].strftime('%Y-%m-%d')+" : "+publi[5]+", "+publi[4]+";")
-
-    print()
-    print("OLDEST SCRAWLED PUBLICATION DATE : "+ publicationsDf.values[0][2].strftime('%Y-%m-%d'))
-
-    # retrieval of user data from the DB
-
-    usersDf = pandas.read_sql(("SELECT * FROM user_newsletter "),newsletter_db)
-
-
-    # LOOP on all users to test if they have at least 3 publications concerning them
-    print()
-    print()
-    print()
-    print("START OF THE PROCESS OF DETECTING USERS TO NOTIFY AND SEND MAILS")
-    print()
-    usersToNotify = []
-    for user in usersDf.values :
-
-    # treatment for one user
-        userId = user[0]
-        userName = user[1]
-        userPhone = user[2]
-        userEmail = user[3]
-        userCategory =user[4]
-        userDate = user[11]
+    if publicationsDf.shape[0]>0 :
+        print('ALL PUBLICATIONS ALLREADY SCRAWLED ORDER BY DATE')
         print()
-        # detecting what are the categories that interest him
-        userCategories =''
-        if userCategory == '000':
-            userCategories = "p.category = '000'"
-        if userCategory == '001' :
-            userCategories = " p.category ='001' or p.category = '111' "
-        if userCategory == '010' :
-            userCategories = " p.category ='010' or p.category = '111' "
-        if userCategory == '100' :
-            userCategories = " p.category ='100' or p.category = '111' "
-        if userCategory == '011' :
-            userCategories = " p.category ='001' or p.category ='010' or p.category = '111' "
-        if userCategory == '101' :
-            userCategories = " p.category ='001' or p.category ='100' or p.category = '111' "
-        if userCategory == '110' :
-            userCategories = " p.category ='100' or p.category ='010' or p.category = '111' "
-        if userCategory == '111' :
-            userCategories = " p.category ='001' or p.category ='010'or p.category ='100' or p.category = '111' "
+        for publi in publicationsDf.values :
+            print(publi[2].strftime('%Y-%m-%d')+" : "+publi[5]+", "+publi[4]+";")
+
+        print()
+        print("OLDEST SCRAWLED PUBLICATION DATE : "+ publicationsDf.values[0][2].strftime('%Y-%m-%d'))
+
+        # retrieval of user data from the DB
+
+        usersDf = pandas.read_sql(("SELECT * FROM user_newsletter "),newsletter_db)
 
 
-        print('ID : ' + userId + ', NAME : ' + userName + ', CATEGORY : ' + userCategory + ', DATE CREATION : '+ userDate.strftime('%Y-%m-%d') +';' )
-        print(" CATEGORIES : " + userCategories)
+        # LOOP on all users to test if they have at least 3 publications concerning them
+        print()
+        print()
+        print()
+        print("START OF THE PROCESS OF DETECTING USERS TO NOTIFY AND SEND MAILS")
+        print()
+        usersToNotify = []
+        for user in usersDf.values :
 
-        # we load all the publications that interest him and he never receive from the newsletter
-        userPublicationsDf = pandas.read_sql(("SELECT * FROM publication as p WHERE ("+ userCategories +") and creation_date > '"+userDate.strftime('%Y-%m-%d')+"' and p.id not in ( SELECT news_id from flags where user_id = '"+ userId +"') ORDER BY creation_date "),newsletter_db)
+        # treatment for one user
+            userId = user[0]
+            userName = user[1]
+            userPhone = user[2]
+            userEmail = user[3]
+            userCategory =user[4]
+            userDate = user[11]
+            print()
+            # detecting what are the categories that interest him
+            userCategories =''
+            if userCategory == '000':
+                userCategories = "p.category = '000'"
+            if userCategory == '001' :
+                userCategories = " p.category ='001' or p.category = '111' "
+            if userCategory == '010' :
+                userCategories = " p.category ='010' or p.category = '111' "
+            if userCategory == '100' :
+                userCategories = " p.category ='100' or p.category = '111' "
+            if userCategory == '011' :
+                userCategories = " p.category ='001' or p.category ='010' or p.category = '111' "
+            if userCategory == '101' :
+                userCategories = " p.category ='001' or p.category ='100' or p.category = '111' "
+            if userCategory == '110' :
+                userCategories = " p.category ='100' or p.category ='010' or p.category = '111' "
+            if userCategory == '111' :
+                userCategories = " p.category ='001' or p.category ='010'or p.category ='100' or p.category = '111' "
 
-        print("PUBLICATIONS THAT INTERESTS  "+ userName +" : " + str(userPublicationsDf.shape[0]))
-        for userpubs in userPublicationsDf.values :
-            print(userpubs[2].strftime('%Y-%m-%d')+" : "+ userpubs[4] + " , " + userpubs[5] )
 
-        # We check if there are at least 3 publications that interest him
-        if( userPublicationsDf.shape[0] > 2 ):
-            usersToNotify += [user]
-            print("At least 3 publications so we send an email to : " + userName)
-            publications = []
-            # we collect the 3 oldest publications
-            for i in range(0,3):
-                publications.append(userPublicationsDf.values[i])
+            print('ID : ' + userId + ', NAME : ' + userName + ', CATEGORY : ' + userCategory + ', DATE CREATION : '+ userDate.strftime('%Y-%m-%d') +';' )
+            print(" CATEGORIES : " + userCategories)
 
-            # sending of the 3 publication to the user
-            NS.send_newsletter(userName, userEmail, publications)
+            # we load all the publications that interest him and he never receive from the newsletter
+            userPublicationsDf = pandas.read_sql(("SELECT * FROM publication as p WHERE ("+ userCategories +") and creation_date > '"+userDate.strftime('%Y-%m-%d')+"' and p.id not in ( SELECT news_id from flags where user_id = '"+ userId +"') ORDER BY creation_date "),newsletter_db)
 
-            # insertion of the 3 flags corresponding for the user and the 3 publications.
-            for i in range (0,3):
-                hash_object = hashlib.md5((userId + userPublicationsDf.values[i][0]).encode())
-                id = hash_object.hexdigest()
-                flagsArray = np.array([[id,userId,userPublicationsDf.values[i][0]]])
-                flagsDf = pandas.DataFrame(flagsArray,
-                                           columns=['id', 'user_id', 'news_id'])
-                flagsDf.to_sql('flags', newsletter_db, if_exists='append', index=False)
+            print("PUBLICATIONS THAT INTERESTS  "+ userName +" : " + str(userPublicationsDf.shape[0]))
+            for userpubs in userPublicationsDf.values :
+                print(userpubs[2].strftime('%Y-%m-%d')+" : "+ userpubs[4] + " , " + userpubs[5] )
 
-    print("USERS NOTIFIED : ")
-    for user in usersToNotify :
-        print(user[1] +" : "+ user[2]+" , " + user[3] +" , " + user[4]+ " ; " )
+            # We check if there are at least 3 publications that interest him
+            if( userPublicationsDf.shape[0] > 2 ):
+                usersToNotify += [user]
+                print("At least 3 publications so we send an email to : " + userName)
+                publications = []
+                # we collect the 3 oldest publications
+                for i in range(0,3):
+                    publications.append(userPublicationsDf.values[i])
+
+                # sending of the 3 publication to the user
+                NS.send_newsletter(userName, userEmail, publications)
+
+                # insertion of the 3 flags corresponding for the user and the 3 publications.
+                for i in range (0,3):
+                    hash_object = hashlib.md5((userId + userPublicationsDf.values[i][0]).encode())
+                    id = hash_object.hexdigest()
+                    flagsArray = np.array([[id,userId,userPublicationsDf.values[i][0]]])
+                    flagsDf = pandas.DataFrame(flagsArray,
+                                               columns=['id', 'user_id', 'news_id'])
+                    flagsDf.to_sql('flags', newsletter_db, if_exists='append', index=False)
+
+        print("USERS NOTIFIED : ")
+        for user in usersToNotify :
+            print(user[1] +" : "+ user[2]+" , " + user[3] +" , " + user[4]+ " ; " )
+    else :
+        print("there is no newsletter to send")
